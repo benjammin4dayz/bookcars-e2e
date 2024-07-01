60c1386ed55d4016d09cf9294f7bea0a797d4dbc
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-07-01 00:38:52 -0400

    Check endpoints before testing

--- cypress/support/global/beforeAll.js
+++ cypress/support/global/beforeAll.js
@@ -1,7 +1,21 @@
 /* eslint-disable mocha/no-top-level-hooks */
+import { env } from '../bookcars';
+
 before(() => {
   cy.log('[DEBUG]: Checking for network connectivity...');
-  cy.request('https://example.com').then(response => {
-    expect(response.status).to.eq(200);
+
+  const req = url => cy.request({ url, failOnStatusCode: false });
+
+  req(env.API_URL).then(res => {
+    expect(res.status).to.eq(404);
+    expect(res.body).to.include('Cannot GET /');
+  });
+
+  req(env.BACKEND_URL).then(res => {
+    expect(res.status).to.eq(200);
+  });
+
+  req(env.FRONTEND_URL).then(res => {
+    expect(res.status).to.eq(200);
   });
 });
